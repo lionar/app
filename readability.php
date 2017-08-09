93b6a373ab2940016a70caf6c0728a7e958e4afd
Aron Wouters <lionar@mail.com>
2017-08-09 11:27:38 +0200

    Changes app facade to  instance

--- readability.php
+++ readability.php
@@ -4,7 +4,8 @@ use Closure as closure;
 
 function when ( string $feature, closure $action )
 {
-	app::binding ( $feature, $action );
+	global $app;
+	$app->binding ( $feature, $action );
 }
 
 function then ( $a )
